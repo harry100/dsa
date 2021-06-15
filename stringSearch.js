47295d69ad1a66bffdaa352b9efcc86a4ac94813
harry100 <maharjanamanda@gmail.com>
2021-06-15 15:10:43 +0545

    simple string search

---
+++ stringSearch.js
@@ -0,0 +1,11 @@
+const naiveSearch = (str, sub) => {
+	let count = 0
+	for (let i=0; i<str.length; i++) {
+		for (let j=0; j< sub.length; j++) {
+			if (str[i + j] !== sub[j]) break
+			if (j === sub.length - 1) count++
+		}
+	}
+	return count
+}
+console.log(naiveSearch("lolabslol","lo"))
