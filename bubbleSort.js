c1b7cf2a62a79aaf1efaae584131477ad6b92850
harry100 <maharjanamanda@gmail.com>
2021-06-28 23:11:06 +0545

    optimization

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,11 +1,15 @@
 const bubble = (arr) => {
 	for (let i=arr.length; i>=0; i--) {
+		let notSwapped = true
 		for (let j=0; j<i-1; j++) {
 			if (arr[j] > arr[j+1]) {
-				[arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+				[arr[j], arr[j+1]] = [arr[j+1], arr[j]]			
+				notSwapped = false
 			}
 		}
+		if (notSwapped) break
 	}
 	return arr
 }
+
 console.log(bubble([1,17,15,2,5,3,1,4]))
