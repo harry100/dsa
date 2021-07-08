dc2514af58c88b123c4326280581234f9d8c3104
harry100 <maharjanamanda@gmail.com>
2021-07-08 22:31:15 +0545

    arg passed outside file

--- selectionSort.js
+++ selectionSort.js
@@ -1,12 +1,13 @@
-const selectSort = (arr) => {
+const selectSort = () => {
+	let arr = process.argv[2].split(',').map(num => parseInt(num))
 	for (let i=0; i<arr.length; i++) {
-		let min = arr[i]
+		let min = i
 		for (let j=i+1; j<arr.length; j++) {
-			if (arr[j] < min) {
+			if (arr[j] < arr[min]) {
 				[arr[j], arr[i]] = [arr[i], arr[j]]
 			}
 		}
 	}
 	return arr
 }
-console.log(selectSort([2,1,4,3,5,5,3,6,1]))
+console.log(selectSort([12,1,14,345,12,143]))
