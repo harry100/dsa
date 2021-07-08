3a6cc1703e973e99d279e03263f4dc2da1be2d36
harry100 <maharjanamanda@gmail.com>
2021-07-08 22:59:34 +0545

    another soln with indices

--- selectionSort.js
+++ selectionSort.js
@@ -10,4 +10,22 @@ const selectSort = () => {
 	}
 	return arr
 }
-console.log(selectSort([12,1,14,345,12,143]))
+
+const selectionSort = () => {
+	let arr = process.argv[2].split(',').map(num => parseInt(num))
+	for (let i=0; i<arr.length; i++) {
+		let lowest = i
+		for (let j=i+1; j<arr.length; j++) {
+			if(arr[j] < arr[lowest]) {
+				lowest = j
+			}
+		}
+		if (i !== lowest) {
+			let temp = arr[i]
+			arr[i] = arr[lowest]
+			arr[lowest] = temp
+		}
+	}
+	return arr
+}
+console.log(selectionSort())
